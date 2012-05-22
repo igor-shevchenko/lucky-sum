cf367a61cce47689baecff8c410bec6c1f653005
Igor Shevchenko <phar0s@ya.ru>
2012-05-22 12:34:44 +0500

    initial filling of array

--- lucky-sum.py
+++ lucky-sum.py
@@ -1,29 +1,36 @@
 from operator import add, sub, mul, div
 
 def operation_results(a, b):
-    concat_numbers = lambda x, y : int(str(x) + str(y))
-    operations = ((add, '+'), (sub, '-'), (mul, '*'), (div, '/'), (concat_numbers, ''))
+    operations = ((add, '+'), (sub, '-'), (mul, '*'), (div, '/'))
     for operation, sign in  operations:
-        yield (operation(a[0], b[0]), ''.join(['(', a[1], sign, b[1], ')']))
+        try:
+            yield (operation(a[0], b[0]), ''.join(['(', a[1], sign, b[1], ')']))
+        except ZeroDivisionError:
+            pass
 
 def find_lucky_sum(number):
-    n = len(str(number))
+    number = str(number)
+    n = len(number)
     d = []
 
     for i in xrange(n):
         d.append([])
-    for digit in map(int, str(number)):
-        d[0].append([])
-        d[0][-1].append((digit, str(digit)))
+        for j in xrange(n - i):
+            d[i].append([])
+
+    for i in xrange(n):
+        for j in xrange(n - i):
+            d[j][i].append((int(number[i:i+j + 1]), number[i:i+j + 1]))
 
     for i in xrange(1, n):
         for j in xrange(n - i):
-            d[i].append([])
+            for lhs in d[i - 1][j]:
+                for rhs in d[i - 1][j + 1]:
+                    for res in operation_results(lhs, rhs):
+                        pass#d[i][j].append(res)
 
     return d
 
 
 if __name__ == '__main__':
-    for lol, lolo in operation_results((12, '12'), (2, '2')):
-        print lol, lolo
-    print find_lucky_sum(123456)
+    print find_lucky_sum(1234)
