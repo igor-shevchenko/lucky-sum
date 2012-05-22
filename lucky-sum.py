83bf0c50a09c2abd9de08e15721450730e2ce4c8
Igor Shevchenko <phar0s@ya.ru>
2012-05-22 14:32:42 +0500

    it works bad

--- lucky-sum.py
+++ lucky-sum.py
@@ -1,12 +1,17 @@
-from operator import add, sub, mul, div
-
 def operation_results(a, b):
+    from operator import add, sub, mul, div
     operations = ((add, '+'), (sub, '-'), (mul, '*'), (div, '/'))
     for operation, sign in  operations:
         try:
             yield (operation(a[0], b[0]), ''.join(['(', a[1], sign, b[1], ')']))
         except ZeroDivisionError:
             pass
+
+#def filter_bad_values(a):
+#    from math import modf
+#    res = []
+#    for i in a:
+#        if modf(a[0])[0]
 
 def find_lucky_sum(number):
     number = str(number)
@@ -20,17 +25,27 @@ def find_lucky_sum(number):
 
     for i in xrange(n):
         for j in xrange(n - i):
-            d[j][i].append((int(number[i:i+j + 1]), number[i:i+j + 1]))
+            d[j][i].append((float(number[i:i+j + 1]), number[i:i+j + 1]))
 
     for i in xrange(1, n):
         for j in xrange(n - i):
             for lhs in d[i - 1][j]:
-                for rhs in d[i - 1][j + 1]:
+                for rhs in d[0][j + i]:
                     for res in operation_results(lhs, rhs):
-                        pass#d[i][j].append(res)
+                        d[i][j].append(res)
 
-    return d
+            if i == 1:
+                # in this case the next cycle is the same as first
+                continue
+
+            for rhs in d[i - 1][j + 1]:
+                for lhs in d[0][j]:
+                    for res in operation_results(lhs, rhs):
+                        d[i][j].append(res)
+
+    return len(d[-1][0])
 
 
 if __name__ == '__main__':
-    print find_lucky_sum(1234)
+    f = open('lol.txt', 'w')
+    f.write(str(find_lucky_sum(123456)))
