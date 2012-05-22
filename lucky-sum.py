5e1a4f8d09aad255a695dcd30c5f0f488b2a144e
Igor Shevchenko <phar0s@ya.ru>
2012-05-22 12:14:29 +0500

    operation_results

--- lucky-sum.py
+++ lucky-sum.py
@@ -1,20 +1,29 @@
 from operator import add, sub, mul, div
 
-concat_numbers = lambda x, y : int(str(x) + str(y))
+def operation_results(a, b):
+    concat_numbers = lambda x, y : int(str(x) + str(y))
+    operations = ((add, '+'), (sub, '-'), (mul, '*'), (div, '/'), (concat_numbers, ''))
+    for operation, sign in  operations:
+        yield (operation(a[0], b[0]), ''.join(['(', a[1], sign, b[1], ')']))
 
 def find_lucky_sum(number):
     n = len(str(number))
     d = []
-    operations = ((add, '+'), (sub, '-'), (mul, '*'), (div, '/'))
+
     for i in xrange(n):
         d.append([])
     for digit in map(int, str(number)):
-        d[0].append(digit)
+        d[0].append([])
+        d[0][-1].append((digit, str(digit)))
 
     for i in xrange(1, n):
-        for j in xrange(n - 1):
-            for operation, sign in operations:
-                pass
+        for j in xrange(n - i):
+            d[i].append([])
+
+    return d
+
 
 if __name__ == '__main__':
+    for lol, lolo in operation_results((12, '12'), (2, '2')):
+        print lol, lolo
     print find_lucky_sum(123456)
