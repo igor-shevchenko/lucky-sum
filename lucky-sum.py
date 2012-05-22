ce2048556fa087aa5097875fa3f96a488fa3f42d
Igor Shevchenko <phar0s@ya.ru>
2012-05-22 11:57:22 +0500

    concat_numbers

--- lucky-sum.py
+++ lucky-sum.py
@@ -1,8 +1,20 @@
+from operator import add, sub, mul, div
 
+concat_numbers = lambda x, y : int(str(x) + str(y))
 
+def find_lucky_sum(number):
+    n = len(str(number))
+    d = []
+    operations = ((add, '+'), (sub, '-'), (mul, '*'), (div, '/'))
+    for i in xrange(n):
+        d.append([])
+    for digit in map(int, str(number)):
+        d[0].append(digit)
 
-
-
+    for i in xrange(1, n):
+        for j in xrange(n - 1):
+            for operation, sign in operations:
+                pass
 
 if __name__ == '__main__':
-    pass
+    print find_lucky_sum(123456)
